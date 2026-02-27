aeaa453223c3f0687ff9461dfb0cfdcf212480f0
witch <witchrainforest@gmail.com>
2026-02-27 17:46:24 +0000

    fix - can run on phone

---
+++ connectToNightCamWifi.sh
@@ -0,0 +1,3 @@
+
+# can find the address with traceroute
+ssh pi@192.168.12.1
---
+++ project/runApp.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# can start the app from the terminal : ) 
+
+set -e
+adb shell monkey -p com.example.compose.jetchat 1
